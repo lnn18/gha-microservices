fe6506f4b57f97a72689f8904fc6a1677ce48231
LinaFigueredo <linis9418@gmail.com>
2025-12-08 20:22:48 -0500

    add workflow

---
+++ src/test/java/la/cloudcamp/demo_cicd/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package la.cloudcamp.demo_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
